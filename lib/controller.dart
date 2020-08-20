6f23f0fa4443ff545834ed1474d5c9bb33a0faa1
Ricardo Conceicao <52796743+rlucioda@users.noreply.github.com>
2020-08-20 06:29:14 -0300

    first commit - exemplo básico do mobx - Counter

--- lib/controller.dart
+++ lib/controller.dart
@@ -1,12 +1,12 @@
 import 'package:mobx/mobx.dart';
-// Include generated file
+// *Include generated file
 part 'controller.g.dart';
 
-// This is the class used by rest of your codebase
+// * This is the class used by rest of your codebase
 class Controller = ControllerBase with _$Controller;
 
 abstract class ControllerBase with Store {
-  // inicia o Observable
+  // *inicia o Observable
   @observable
   int counter = 0;
 
